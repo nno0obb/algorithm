8b319d2db18d8c7dd79d44695eb0174925a47178
nno0obb <nno0obb@kakao.com>
2026-02-10 22:50:01 +0900

    boj/10814

---
+++ problem_solving/boj/10814/soln10814.py
@@ -0,0 +1,26 @@
+"""
+# 백준
+# No. 10814
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+import sys
+from itertools import chain
+
+
+def main():
+    # Input
+    N = int(input())
+    A = [sys.stdin.readline().split() for _ in range(N)]
+
+    # Logic
+    A.sort(key=lambda x: int(x[0]))
+
+    # Output
+    for a in A:
+        print(*a)
+
+
+if __name__ == "__main__":
+    main()
