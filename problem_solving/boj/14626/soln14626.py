d96b896e8aff6d83bea69f6a14a5be001aaceafe
nno0obb <nno0obb@kakao.com>
2026-02-23 22:44:16 +0900

    boj/14626

--- problem_solving/boj/14626/soln14626.py
+++ problem_solving/boj/14626/soln14626.py
@@ -11,13 +11,28 @@ def main():
     ISBN = input()
 
     # Logic
-    remainder = 0
-    for i, num in enumerate(ISBN):
-        weight = 1 + (i % 2) * 2
+    remainder, idx_damaged = 0, -1
+    for idx, num in enumerate(ISBN):
+        weight = 1 + (idx % 2) * 2
         if num.isdigit():
             remainder += int(num) * weight
+        elif num == "*":
+            idx_damaged = idx
     remainder %= 10
     ans = 10 - remainder if remainder != 0 else 0
+    if idx_damaged % 2 == 1:
+        ans = {
+            0: 0,
+            1: 7,
+            2: 4,
+            3: 1,
+            4: 8,
+            5: 5,
+            6: 2,
+            7: 9,
+            8: 6,
+            9: 3,
+        }[ans]
 
     # Output
     print(ans)
